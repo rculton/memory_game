3c0db4499d25c683ce78400f09dba8fc9752a695
rculton <rculton@gmail.com>
2017-08-18 09:25:03 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,25 +2,29 @@ console.log("Up and running");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo;
-cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
+var flipCard = function(cardID){
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-
-if(cardsInPlay.length === 2)
-{
-	if( cardsInPlay[0] === cardsInPlay[1])
+};
+var checkMatch = function(){
+	if(cardsInPlay.length === 2)
 	{
-		alert("You found a match!");
-	}
-	else{
-		alert("Sorry! Try again!");
-	}
-	cardsInPlay.pop();
-	cardsInPlay.pop();
-}
+		if( cardsInPlay[0] === cardsInPlay[1])
+		{
+			alert("You found a match!");
+			cardsInPlay.pop();
+			cardsInPlay.pop();
+		}
+		else{
+			alert("Sorry! Try again!");
+			cardsInPlay.pop();
+			cardsInPlay.pop();
+		}
+		
+	}};
+
+flipCard(0);
+flipCard(2);
+checkMatch();
+
